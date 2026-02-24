52393c83bada12d18a1d65f2d1e229e77320a730
Rogélio <rogelio.claro@gmail.com>
2026-02-23 21:31:30 -0300

    feat: implementar temas, ícones e gerenciamento de estado
    - Criação do arquivo global de temas para padronização de cores.
    - Integração da biblioteca MaterialIcons para os campos de input.
    - Evolução do layout com inputs arredondados e estilização avançada.
    - Implementação de interatividade com o componente TouchableOpacity.
    - Adição de lógica de estado com o hook useState para e-mail e senha.
    - Configuração de secureTextEntry para proteção do campo de senha.

---
+++ src/global/themes.tsx
@@ -0,0 +1,12 @@
+export const themas = {
+  colors: {
+    primary: "#878af6",
+    secondary: "#ffffff",
+    lighGray: "#d7d8d7",
+    gray: "gray",
+    black: "#000000",
+    white: "#ffffff",
+    bgScreen: "#f1f7fa",
+    corDeFundo: "#a4c639",
+  },
+};
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,26 +1,50 @@
-import React from "react";
-import { Text, View, Image, TextInput } from "react-native";
+import React, { useState } from "react";
+import { Text, View, Image, TextInput, TouchableOpacity } from "react-native";
 import { style } from "./styles";
+import { MaterialIcons } from "@expo/vector-icons";
+import { themas } from "../../global/themes";
 import Logo from "../../assets/login-9.png";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
   return (
     <View style={style.container}>
       <View style={style.boxTop}>
         <Image source={Logo} />
-        <Text>Bem Vindo!!!</Text>
+        <Text style={{ fontWeight: "bold", marginTop: 10 }}>Bem Vindo!!!</Text>
       </View>
 
       <View style={style.boxMid}>
-        <Text>Endereço de E-mail</Text>
-        <TextInput style={style.boxInput} />
+        <Text style={style.title}>E-mail</Text>
+        <View style={style.boxInputEmail}>
+          <TextInput
+            style={style.input}
+            value={email}
+            onChangeText={(e) => setEmail(e)}
+          />
+          <MaterialIcons name="email" size={20} color={themas.colors.gray} />
+        </View>
 
-        <Text>Coloque sua Senha</Text>
-        <TextInput style={style.boxInput} />
+        <Text style={style.title}>Coloque sua Senha</Text>
+        <View style={style.boxInputSenha}>
+          <TextInput
+            style={style.input}
+            value={senha}
+            onChangeText={(e) => setSenha(e)}
+            secureTextEntry={true}
+          />
+          <MaterialIcons name="password" size={20} color={themas.colors.gray} />
+        </View>
       </View>
 
       <View style={style.boxBottom}>
-        <Text>Bottom</Text>
+        <TouchableOpacity style={style.button}>
+          <Text style={{ color: themas.colors.secondary, fontWeight: "bold" }}>
+            Entrar
+          </Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
